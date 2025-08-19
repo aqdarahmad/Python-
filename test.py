04fe71d086704194f75f64abb7a125a615dac8b3
aqdarahmad <a.abusufyan@student.aaup.edu>
2025-08-19 22:02:14 +0300

    file

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("Your name",input("Enter ur name"),"And your age: ", input("Enter ur age: "))
+#print("Your name",input("Enter ur name"),"And your age: ", input("Enter ur age: "))
+
+
+
+# input / output files 
+# w--> create new file and print it 
+# with --> must tap before write code
+with open ("file.txt" , "r") as f:
+    f.read()
+#with open("file.txt","w") as f:
+    
